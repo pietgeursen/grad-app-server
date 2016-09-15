2c7470b899aeb048d3e0495f8b85f048e6474c8f
Piet Geursen <pietgeursen@gmail.com>
2016-09-15 15:07:56 +1200

    Try adding lex stuff.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 'use strict'
 
 const app = require('./app')()
-app.set('port', process.env.PORT || 3030)
-const port = app.get('port')
-const server = app.listen(port)
+const port = process.env.PORT || 3030
+const sport = (process.env.NODE_ENV === 'production') ? 443 : 3031
 
-server.on('listening', () => console.log(`Feathers application started on ${app.get('host')}:${port}`)
-)
+const server = require('letsencrypt-express').create({
+  server: 'staging',
+  email: 'pietgeursen@gmail.com',
+  agreeTos: true,
+  approveDomains: [ 'devacademygrads.co.nz' ],
+  app: app
+}).listen(port, sport)
+
+server.on('listening', () => console.log(`Feathers application started on ${app.get('host')}:${port}`))
